5f0863795815221ddefdf1a28ebcae5998ed2b3e
Matheus Marques <77027053+matheus258@users.noreply.github.com>
2021-05-09 10:18:17 -0300

    Update 1071 - Soma de Impares Consecutivos.py
    Substituir os sinais..  - por +.

--- 1071 - Soma de Impares Consecutivos.py
+++ 1071 - Soma de Impares Consecutivos.py
@@ -11,18 +11,18 @@ y = int(input())
 
 # Logica
 if x > y:
-    x -= 1
+    x += 1
     while x > y:
         if x % 2 != 0:
             soma += x
-        x -= 1
+        x += 1
 
 if y > x:
-    y -= 1
+    y += 1
     while y > x:
         if y % 2 != 0:
             soma += y
-        y -= 1
+        y += 1
 
 # Saida
 print("%d" % soma)
